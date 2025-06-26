00204ba9eab606233f1b75ee3f7414335f60ebcf
NaveenReddy2004 <naveen18659@gmail.com>
2025-06-26 12:52:46 +0530

    Update resume_loader.py

--- resume_loader.py
+++ resume_loader.py
@@ -1,6 +1,6 @@
 import fitz  # PyMuPDF
 
-def load_resume_text(file_path='resume.pdf'):
+def load_resume_text(file_path='Naveen_Resume(2).pdf'):
     doc = fitz.open(file_path)
     text = ""
     for page in doc:
