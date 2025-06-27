8c377aaf6b3327643c5f7c7fd52820d35790fe87
NaveenReddy2004 <naveen18659@gmail.com>
2025-06-27 15:11:44 +0530

    Update resume_loader.py

--- resume_loader.py
+++ resume_loader.py
@@ -1,9 +1,12 @@
-import fitz  # PyMuPDF
+import os
+import fitz
 
-def load_resume_text(file_path='Naveen_Resume(2).pdf'):
+def load_resume_text(file_path="resumes/Naveen_Resume.pdf"):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Resume file not found at: {file_path}")
+
     doc = fitz.open(file_path)
     text = ""
     for page in doc:
         text += page.get_text()
     return text
-
